ad6bb80cad4b8dc3cb544c4748d8d672c90ae1c1
Yulia Solomakha <yulichka700@gmail.com>
2023-11-28 21:22:53 +0100

    Добавьте файлы проекта.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5292");
+
+//var input = new HelloRequest { Name = "Yuliia" };
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(input);
+//Console.WriteLine(reply.Message);
+
+var customerClient = new Customer.CustomerClient(channel);
+
+var clientRequest = new CustomerLookupModel { UserId = 2 };
+
+var customer = await customerClient.GetCustomerInfoAsync(clientRequest);
+
+Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+Console.WriteLine("New customers list");
+
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+    }
+}
+Console.ReadLine();
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,82 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Yuliia";
+                output.LastName = "Solomakha";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Vlad";
+                output.LastName = "Spotar";
+            }
+            else if (request.UserId == 3)
+            {
+                output.FirstName = "Dmytro";
+                output.LastName = "Aleksenko";
+            }
+            else
+            {
+                output.FirstName = "Julia";
+                output.LastName = "Nedavnya";
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public async override Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Alina",
+                    LastName = "Bedenko",
+                    EmailAddress = "alina@gmail.com",
+                    Age = 21,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Dmytro",
+                    LastName = "Lytvynenko",
+                    EmailAddress = "d_lytv@gmail.com",
+                    Age = 20,
+                    IsAlive = true
+                },
+                 new CustomerModel
+                {
+                    FirstName = "Karina",
+                    LastName = "Gurinenko",
+                    EmailAddress = "guri@gmail.com",
+                    Age = 22,
+                    IsAlive = true
+                }
+
+            };
+
+            foreach (var custumer in customers)
+            {
+                await responseStream.WriteAsync(custumer);
+            }
+        }
+    }
+
+     
+    
+}
